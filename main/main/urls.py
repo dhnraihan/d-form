d83890a4784d036e5ec3ebbb123989c395f800a4
Dhn_Raihan <30395903+RHNRaihan@users.noreply.github.com>
2025-01-09 20:52:02 +0600

    added app1,2,3,4,5

--- main/main/urls.py
+++ main/main/urls.py
@@ -21,4 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('app1/', include('app1.urls')),
+    path('app2/', include('app2.urls')),
+    path('app3/', include('app3.urls')),
+    path('app4/', include('app4.urls')),
+    path('app5/', include('app5.urls')),
 ]
